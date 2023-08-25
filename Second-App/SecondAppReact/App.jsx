312660cee410b84948d1a2e11ea1ee700ee28fe2
josephyv <haguilar@auronix.com>
2023-08-25 15:46:04 -0600

    creating project

---
+++ Second-App/SecondAppReact/App.jsx
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  Pressable,
+  StyleSheet,
+  View,
+  TextInput,
+} from 'react-native';
+
+const App = () => {
+  const [nombre, setNombre] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  return (
+    <SafeAreaView style={[styles.screen]}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('./images/LIS.png')} />
+      </View>
+      <View style={styles.formulary}>
+        <Text style={[styles.title]}>Tu Nombre:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe Aquí"
+          value={nombre}
+          onChangeText={setNombre}
+        />
+        <View style={styles.campoMensaje}>
+          <Text style={[styles.mensaje, styles.title]}>{mensaje}</Text>
+        </View>
+        <Pressable
+          style={styles.btnPressable}
+          onPress={() => {
+            let messaje = `Hola ${nombre.toUpperCase()} bienvenido al curso DDA`;
+            if (!nombre) {
+              messaje = '';
+            }
+            setMensaje(messaje);
+          }}>
+          <Text style={styles.btnText}>Saludar</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  formulary: {
+    marginVertical: 15,
+    marginHorizontal: 10,
+  },
+  imageContainer: {
+    alignItems: 'center',
+  },
+  image: {
+    width: '70%',
+    resizeMode: 'contain',
+  },
+  screen: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  btnPressable: {
+    backgroundColor: '#0000ff',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+  },
+  btnText: {
+    color: '#FFF',
+    fontSize: 30,
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  campoMensaje: {
+    marginTop: 20,
+    marginBottom: 60,
+    marginHorizontal: 10,
+  },
+  input: {
+    backgroundColor: '#DDDDDD',
+    color: '#222222',
+    fontSize: 30,
+    padding: 12,
+    borderRadius: 10,
+  },
+  mensaje: {
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 30,
+    color: '#000',
+  },
+});
+
+export default App;
